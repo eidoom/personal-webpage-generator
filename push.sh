5b0902103ffb4829ff88a378a49496d80437ee8e
Ryan Moodie <ryanmoodie@gmail.com>
2019-12-06 13:11:33 +0000

    Update

--- push.sh
+++ push.sh
@@ -5,15 +5,15 @@ echo -e "\n# Moving to public\n"
 cd public
 git add .
 git status --short
-echo -e "\n"
+echo
 git commit -m "Update"
 git push
 echo -e "\n# Moving back up to root\n"
 cd ..
 git add -u
 git status --short
-echo -e "\n"
+echo
 git commit -m "Update"
 git push
-echo -e "\n"
+echo
 git status
