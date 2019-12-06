2724ce0cf2b60be670ea537d0d9ea970c7f03b21
Ryan Moodie <ryanmoodie@gmail.com>
2019-12-06 13:09:06 +0000

    Update

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 hugo
-echo Moving to public\n
+echo -e \nMoving to public\n
 cd public
 git add .
 git status --short
 git commit -m "Update"
 git push
-echo Moving back up to root
+echo -e \nMoving back up to root\n
 cd ..
 git add -u
 git status --short
